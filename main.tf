5e3c0e3793089799d9d6683101e14796d0b5beb2
Vinay K <johnvinayjvk2895@gmail.com>
2026-08-09 21:24:39 +0530

    Comment Terraform module definitions in main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 # module "vpc" {
-  source = "./modules/vpc"
+ # source = "./modules/vpc"
 }
 
 # module "sg" {
-  source  = "./modules/sg"
-  sg_name = var.sg_name
+ # source  = "./modules/sg"
+ # sg_name = var.sg_name
 }
 
 # module "ec2" {
-  source            = "./modules/ec2"
-  security_group_id = module.sg.sg_id
-  instance_name     = var.instance_name
+#  source            = "./modules/ec2"
+# security_group_id = module.sg.sg_id
+# instance_name     = var.instance_name
 }
 
 

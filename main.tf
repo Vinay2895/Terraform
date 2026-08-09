8e07d2ce6397de9cb16c0ff087163cf57305ca03
Vinay K <johnvinayjvk2895@gmail.com>
2026-08-09 21:25:06 +0530

    comment Terraform module configurations

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
 # module "vpc" {
- # source = "./modules/vpc"
-}
+ # source = "./modules/vpc"}
 
 # module "sg" {
  # source  = "./modules/sg"
- # sg_name = var.sg_name
-}
+ # sg_name = var.sg_name}
 
 # module "ec2" {
 #  source            = "./modules/ec2"
 # security_group_id = module.sg.sg_id
-# instance_name     = var.instance_name
-}
+# instance_name     = var.instance_name}
 
 
 ############################################
